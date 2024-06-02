021bb8cb85d4074d0d123eea4de9db9d1a196bd5
manraf <45131457+MANRAF04@users.noreply.github.com>
2024-06-02 19:57:22 +0100

    Add files via upload

--- set2/prob3.py
+++ set2/prob3.py
@@ -1,8 +1,25 @@
-# A Dynamic Programming based Python
-# Program for 0-1 Knapsack problem
-# Returns the maximum value that can
-# be put in a knapsack of capacity W
+# Names:    Balamotis Panagiotis    Raftopoulos Emmanouil
+# AEM:      03726                   03735
+ 
 
+def printItems(K,W,n):
+    w = W
+    for i in range(n,0,-1):
+        if K[i][w] != K[i-1][w]:
+            print(f"Item {i} with weight {weight[i-1]} and value {profit[i-1]}")
+            w = w - weight[i-1]
+
+def printArray(K):
+    print()
+    print()
+    
+    for i in range(len(K[0])):
+        print("%03d "% (i),end=' ')
+    print()
+    for i in K:
+        for j in i:
+            print("%03d "% (j),end=' ')
+        print()
 
 def knapSack(W, wt, val, n):
     K = [[0 for x in range(W + 1)] for x in range(n + 1)]
@@ -18,17 +35,8 @@ def knapSack(W, wt, val, n):
                               K[i-1][w])
             else:
                 K[i][w] = K[i-1][w]
-
-    for i in range(len(K[0])):
-        #print(f"{i}",end='  ')
-        print("%03d "% (i),end=' ')
-    print()
-    for i in K:
-        for j in i:
-            #print(j,end='  ')
-            print("%03d "% (j),end=' ')
-        print()
-    return K[n][W]
+        printArray(K)
+    return K
 
 
 # Driver code
@@ -37,6 +45,6 @@ if __name__ == '__main__':
     weight = [12,9,5,5,8]
     W = 25
     n = len(profit)
-    print(knapSack(W, weight, profit, n))
-
-# This code is contributed by Bhavya Jain
+    K = knapSack(W, weight, profit, n)
+    print(K[n][W])
+    printItems(K,W,n)
--- set2/prob4.py
+++ set2/prob4.py
@@ -1,4 +1,6 @@
-# Example usage
+# Names:    Balamotis Panagiotis    Raftopoulos Emmanouil
+# AEM:      03726                   03735
+ 
 gains = [
     [1.5, 3.0, 4.2, 5.0, 5.6],
     [1.4, 2.6, 3.8, 4.8, 5.9],
@@ -7,11 +9,9 @@ gains = [
 
 def max_gain(i, gain, j):
     if j < 0 or i >= len(gains):
-        print ("i", i, "j", j, "gain", gain) 
+        print (f"Leaving recursion {i}") 
         return 0
     for k in range(j, -1, -1):
-        print("i", i, "j", j, "k", k)
-        print("gain", gain, f"gains[{i}][{k}]", gains[i][k])
         gain = max(gain , gains[i][k] + max(max_gain(i+1, 0, j-k-1), max_gain(i+2, 0, j-k-1)))
         print("gain", gain, "k", k)
     return gain
